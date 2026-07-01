dbd8ee340b0621da666f6fbef521adaeb5c7bc4a
Bharathitamilvel27 <bharathitamilvel27@gmail.com>
2026-07-01 09:37:44 +0530

    Time: 1492 ms (34.4%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -7,3 +7,6 @@ on v.visit_id = t.visit_id
 where t.transaction_id is null
 group by v.customer_id;
  --  where t.transaction_id is null - It keeps only the rows where there was no matching transaction(which means no transaction happened thus no transaction id is there for those vivits)
+
+ -- id they ask those whoo have done transaction means just put inner join & remove the line where t.transaction_id is null
+ -- if they ask those who made atleast 2 transactions - use having - group by v.customer_id , having count(*)>=2
