01fd741b400cfa6113873d1ef59936dd16682951
Bharathitamilvel27 <bharathitamilvel27@gmail.com>
2026-09-11 09:25:19 +0530

    Time: 0 ms (100%), Space: 48.7 MB (50.82%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,8 +1,8 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k=2;
+        int k=2;  //bcz an element can appear 1 or 2 times not more than 2 , if it appears -> duplicate
         for(int i=2;i<nums.length;i++){
-            if(nums[i]!=nums[k-2]){
+            if(nums[i]!=nums[k-2]){ // it checks whether current element is same as the element which is at 2 step before it
                 nums[k]=nums[i];
                 k++;
             }
